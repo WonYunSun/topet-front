7b45f246123514a74f1726d800d75e286e97fa23
WonYunSun <ereunosun113@gmail.com>
2024-07-03 13:36:28 +0900

    feat : calendar component

---
+++ react/src/page/calendar_screen.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Calendar } from "./../component/Calendar";
+import TopBar from "../component/TopBar";
+
+import AnimalSelect from "../component/AnimalSelect";
+import BottomSheet from "../component/BottomSheet";
+import "../css/bottomsheet.css";
+
+export const Calendarscreen = () => {
+  const [showBottomSheet, setShowBottomSheet] = useState(false);
+  const [bottomSheetType, setBottomSheetType] = useState(null);
+  const [selectedPet, setSelectedPet] = useState(null);
+
+  const handleOpenPetBottomSheet = () => {
+    setBottomSheetType("pet");
+    setShowBottomSheet(true);
+  };
+
+  const handleCloseBottomSheet = () => {
+    setShowBottomSheet(false);
+  };
+
+  const handleSelectPet = (pet) => {
+    setSelectedPet(pet);
+    handleCloseBottomSheet();
+  };
+
+  return (
+    <div>
+      <TopBar />
+      <AnimalSelect
+        onClick={handleOpenPetBottomSheet}
+        selectedPet={selectedPet}
+      />
+      <Calendar />
+      <BottomSheet
+        show={showBottomSheet}
+        onClose={handleCloseBottomSheet}
+        onSelectPet={handleSelectPet}
+        type={bottomSheetType}
+      />
+    </div>
+  );
+};
+export default Calendarscreen;
