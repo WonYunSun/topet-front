e8480f55fc13349a02328c063e80554530c9ca3e
WonYunSun <ereunosun113@gmail.com>
2024-07-04 17:04:05 +0900

    feat : addSchedule

--- react/src/component/FloatingBtn.js
+++ react/src/component/FloatingBtn.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "../css/floating_btn.module.css";
 
-export default function FloatingBtn() {
+export default function FloatingBtn({ onClick }) {
   return (
-    <div className={styles.BtnContiner}>
+    <div className={styles.BtnContiner} onClick={onClick}>
       <div className={styles.AddscdBtn}>추가</div>
     </div>
   );
