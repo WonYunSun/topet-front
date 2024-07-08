57bf67dccbe84d738439a3a66a9fc7cd72c4731a
WonYunSun <ereunosun113@gmail.com>
2024-07-08 19:50:26 +0900

    feat : global font fix(woff), schedule box css add

--- react/src/component/ScheduleBottom.js
+++ react/src/component/ScheduleBottom.js
@@ -5,6 +5,11 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 import styles from "../css/schedule_bottom.module.css";
 import "dayjs/locale/ko";
 import BottomSheet from "../component/BottomSheet";
+
+import { GoCircle } from "react-icons/go";
+
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
+import { RxDividerVertical } from "react-icons/rx";
 dayjs.extend(customParseFormat);
 dayjs.extend(localizedFormat);
 dayjs.locale("ko");
@@ -55,7 +60,35 @@ const ScheduleBottom = ({ schedules, selectedDate }) => {
                   style={{ backgroundColor: item.color }}
                   onClick={() => handleScheduleClick(item)}
                 >
-                  {item.scheduleTitle}
+                  <div className={styles.ScheduleBoxContent}>
+                    <div className={styles.ScheduleContent}>
+                      <div className={styles.ScheduleDate}>
+                        {dayjs(item.startDate).format("h:mm a") ===
+                          "12:00 오전" &&
+                        dayjs(item.endDate).format("h:mm a") ===
+                          "11:59 오후" ? (
+                          "하루 종일"
+                        ) : (
+                          <>
+                            {dayjs(item.startDate).format("h:mm a")} -{" "}
+                            {dayjs(item.endDate).format("h:mm a")}
+                          </>
+                        )}
+                      </div>
+                      <div className={styles.ScheduleTitle}>
+                        {item.scheduleTitle}
+                      </div>
+                    </div>
+                    <div className={styles.ScheduleBoxCheckBox}>
+                      <div className={styles.verticalDivider} />
+
+                      {item.isComplete ? (
+                        <IoCheckmarkCircleOutline color="#fff" size={28} />
+                      ) : (
+                        <GoCircle color="rgba(255, 255, 255, 0.3)" size={25} />
+                      )}
+                    </div>
+                  </div>
                 </div>
               ))
             ) : (
