e67477a203a577cba7f1ecc39fa15f10fb14c1b3
WonYunSun <ereunosun113@gmail.com>
2024-07-09 13:40:19 +0900

    fixed : api call

--- react/src/api/scheduleApi.js
+++ react/src/api/scheduleApi.js
@@ -1,24 +1,44 @@
-const API_BASE_URL = "http://175.45.202.131:8081/api"; // 실제 API 엔드포인트로 변경하세요.
+import axios from "axios";
 
-export const updateScheduleStatus = async (scheduleId) => {
-  try {
-    const response = await fetch(
-      `${API_BASE_URL}/schedules/postScheduleId/${scheduleId}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ scheduleId }),
-      }
-    );
+const API_BASE_URL = "http://175.45.202.131:8081/api";
 
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Failed to update schedule status:", error);
-    throw error;
+class ScheduleService {
+  constructor(baseURL) {
+    this.client = axios.create({
+      baseURL: baseURL,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
-};
+
+  async updateScheduleStatus(scheduleId) {
+    try {
+      const response = await this.client.post(
+        `/schedules/postScheduleId/${scheduleId}`,
+        {
+          scheduleId,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Failed to update schedule status:", error);
+      throw error;
+    }
+  }
+
+  async postSche(formData) {
+    try {
+      const response = await this.client.post("/schedule/post", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("서버 응답:", response.data);
+    } catch (error) {
+      console.error("서버 오류:", error);
+    }
+  }
+}
+
+export default new ScheduleService(API_BASE_URL);
