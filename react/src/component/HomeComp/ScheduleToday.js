246aed52946a911ef34fcc03069db5533da466d2
WonYunSun <ereunosun113@gmail.com>
2024-07-16 20:52:22 +0900

    feat : home.js

---
+++ react/src/component/HomeComp/ScheduleToday.js
@@ -0,0 +1,81 @@
+import React, { useCallback, useState, useEffect } from "react";
+import dayjs from "dayjs";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ScheduleBox from "../CalendarComp/scheduleBox";
+import styles from "../../css/scheduleToday.module.css";
+import "dayjs/locale/ko";
+import ScheduleService from "../../api/scheduleApi"; // postApi
+
+function ScheduleToday({ schedules }) {
+  //   useEffect(() => {
+  //     // Fetch today's schedule
+  //     fetch("/api/schedule/today")
+  //       .then((response) => response.json())
+  //       .then((data) => setSchedule(data))
+  //       .catch((error) => console.error("Error fetching schedule:", error));
+  //   }, []);
+  const [updatedSchedules, setUpdatedSchedules] = useState(schedules);
+  const [selectedDate, setSelectedDate] = useState(dayjs());
+
+  useEffect(() => {
+    setUpdatedSchedules(schedules);
+  }, [schedules]);
+
+  const getSchedule = useCallback(
+    (date) => {
+      const day = updatedSchedules.filter((schedule) => {
+        const start = dayjs(schedule.startDate);
+        const end = dayjs(schedule.endDate);
+        const targetDate = dayjs(date);
+
+        return targetDate.isBetween(start, end, "day", "[]");
+      });
+
+      return day.length > 0 ? day : [];
+    },
+    [updatedSchedules]
+  );
+
+  const handleCheckBoxClick = async (scheduleId) => {
+    try {
+      await ScheduleService.updateScheduleStatus(scheduleId);
+      setUpdatedSchedules((prevSchedules) =>
+        prevSchedules.map((schedule) =>
+          schedule.scheduleId === scheduleId
+            ? { ...schedule, isComplete: !schedule.isComplete }
+            : schedule
+        )
+      );
+    } catch (error) {
+      console.error("Failed to update schedule status:", error);
+    }
+  };
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className={styles.scheduleContainer}>
+      {schedules.length > 0 ? (
+        <Slider {...settings}>
+          {schedules.map((item) => (
+            <div key={item.scheduleId}>
+              <ScheduleBox item={item} />
+            </div>
+          ))}
+        </Slider>
+      ) : (
+        <div className={styles.noSchedule}>일정이 없습니다.</div>
+      )}
+    </div>
+  );
+}
+
+export default ScheduleToday;
