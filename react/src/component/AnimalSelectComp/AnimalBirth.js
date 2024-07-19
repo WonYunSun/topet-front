d2dc72e72415046fa7ccded8b13dbf853774655d
HCWDDD <codn1016@gmail.com>
2024-07-19 23:38:15 +0900

    feat: petRegistration css

--- react/src/component/AnimalSelectComp/AnimalBirth.js
+++ react/src/component/AnimalSelectComp/AnimalBirth.js
@@ -2,60 +2,125 @@ import React, { useState, useEffect } from 'react';
 import years from '../../data/year';
 import dayjs from 'dayjs';
 import styles from '../../css/animal_birth.module.css';
+import { TbTriangleInvertedFilled } from "react-icons/tb";
 
 const AnimalBirth = ({ year, month, day, setYear, setMonth, setDay, selectedBirth, setSelectedBirth, setNextPossible, birthDontKnow, setBirthDontKnow }) => {
-    const today = dayjs(); // 현재 날짜를 가져옵니다
-    const currentYear = today.year();
-    let currentMonth = today.month() + 1; // month는 0부터 시작하므로 1을 더합니다
-    const currentDay = today.date();
-    if(birthDontKnow == true){
+    const [dropdown, setDropdown] = useState({
+        year: false,
+        month: false,
+        day: false,
+    });
+    const [openDropdownType, setOpenDropdownType] = useState(null);
+
+    if(birthDontKnow) {
         setNextPossible(true);
     }
+
+    const toggleDropdown = (type) => {
+        if (!birthDontKnow) {
+            setDropdown(prevState => ({
+                year: type === 'year' ? !prevState.year : false,
+                month: type === 'month' ? !prevState.month : false,
+                day: type === 'day' ? !prevState.day : false,
+            }));
+            setOpenDropdownType(type);
+        }
+    };
+
+    const closeDropdown = () => {
+        setDropdown({
+            year: false,
+            month: false,
+            day: false,
+        });
+        setOpenDropdownType(null);
+    };
+
+    useEffect(() => {
+        if (birthDontKnow) {
+            setNextPossible(true);
+        }
+    }, [birthDontKnow, setNextPossible]);
+
     useEffect(() => {
         if (year && month && day) {
             setSelectedBirth(`${year}${String(month).padStart(2, '0')}${String(day).padStart(2, '0')}`);
         }
     }, [year, month, day, setSelectedBirth]);
 
-    const handleYearChange = (e) => {
-        const newYear = e.target.value;
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.target.closest(`.${styles.custom_select}`)) {
+                closeDropdown();
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const today = dayjs();
+    const currentYear = today.year();
+    let currentMonth = today.month() + 1;
+    const currentDay = today.date();
+
+    useEffect(() => {
+        if (birthDontKnow) {
+            setNextPossible(true);
+            setYear('');
+            setMonth('');
+            setDay('');
+            setSelectedBirth('');
+        } else if (year && month && day) {
+            setNextPossible(true);
+        } else {
+            setNextPossible(false);
+        }
+    }, [birthDontKnow, year, month, day, setNextPossible]);
+
+    const handleYearChange = (newYear) => {
         setYear(newYear);
 
         if (newYear && month && day) {
             setSelectedBirth(`${newYear}${String(month).padStart(2, '0')}${String(day).padStart(2, '0')}`);
         }
+
+        closeDropdown();
     };
 
     const renderYearOptions = () => {
-    return years
-        .filter((year) => year <= currentYear)
-        .sort((a, b) => b - a)
-        .map((year, index) => (
-            <option key={index} value={year}>
-                {year}년
-            </option>
-        ));
-};
+        return years
+            .filter((year) => year <= currentYear)
+            .sort((a, b) => b - a)
+            .map((year, index) => (
+                <div key={index} className={styles.option} onClick={() => handleYearChange(year)}>
+                    {year}년
+                </div>
+            ));
+    };
 
-    const handleMonthChange = (e) => {
-        const newMonth = e.target.value;
+    const handleMonthChange = (newMonth) => {
         setMonth(newMonth);
         if (year && newMonth && day) {
             setSelectedBirth(`${year}${String(newMonth).padStart(2, '0')}${String(day).padStart(2, '0')}`);
         }
+
+        closeDropdown();
     };
 
     const renderMonthOptions = () => {
-        const months = Array.from({ length: year == currentYear ? currentMonth : 12 }, (_, i) => i + 1);
+        const months = Array.from({ length: year === currentYear ? currentMonth : 12 }, (_, i) => i + 1);
         return months.map((month) => (
-            <option key={month} value={month}>
+            <div key={month} className={styles.option} onClick={() => handleMonthChange(month)}>
                 {month}월
-            </option>
+            </div>
         ));
     };
 
-    const handleDayChange = (e) => {
-        const newDay = e.target.value;
+    const handleDayChange = (newDay) => {
         setDay(newDay);
         if (year && month && newDay) {
             let tempBirth = `${year}${String(month).padStart(2, '0')}${String(newDay).padStart(2, '0')}`;
@@ -64,76 +129,83 @@ const AnimalBirth = ({ year, month, day, setYear, setMonth, setDay, selectedBirt
             }
             let thisDay = currentYear.toString() + currentMonth.toString() + currentDay.toString();
 
-            if (tempBirth <= thisDay) { //오늘보다 tempBirth가 작으면 가능, 아니면 불가능
+            if (tempBirth <= thisDay) {
                 setNextPossible(true);
                 setSelectedBirth(tempBirth);
             }
         }
+
+        closeDropdown();
     };
 
     const renderDayOptions = () => {
         const days = Array.from(
-            { length: (year == currentYear && month == currentMonth) ? currentDay : dayjs(`${year}-${month}`).daysInMonth() },
+            { length: (year === currentYear && month === currentMonth) ? currentDay : dayjs(`${year}-${month}`).daysInMonth() },
             (_, i) => i + 1
         );
 
         return days.map((day) => (
-            <option key={day} value={day} disabled={day === ''}>
+            <div key={day} className={styles.option} onClick={() => handleDayChange(day)}>
                 {day}일
-            </option>
+            </div>
         ));
     };
 
     const dontKnowToggle = () => {
-        /*if (birthDontKnow) {
-            setBirthDontKnow(false);
-            setNextPossible(false);
-        } else {
-            setBirthDontKnow(true);
-            setNextPossible(true);
+        setBirthDontKnow(prev => !prev);
+        setNextPossible(birthDontKnow);
+
+        if (!birthDontKnow) {
             setYear('');
             setMonth('');
             setDay('');
             setSelectedBirth('');
-        } */
+        }
+    };
 
-            setBirthDontKnow(prev => !prev);
-            setNextPossible(!birthDontKnow);
-        
-            if (!birthDontKnow) {
-                setYear('');
-                setMonth('');
-                setDay('');
-                setSelectedBirth('');
-            }
-    }
+    useEffect(() => {
+        if (birthDontKnow || (year && month && day)) {
+            setNextPossible(true);
+        } else {
+            setNextPossible(false);
+        }
+    }, [birthDontKnow, year, month, day, setNextPossible]);
 
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.title}>생일을 알려주세요</h1>
             <div className={styles.birth_wrapper}>
-                <div className={styles.year_wrapper}>
-                <select className={styles.birth_select} value={year} onChange={handleYearChange} disabled={birthDontKnow}>
-                    <option className={styles.birth_select_option} value="">{(year === '') ? currentYear + '년' : year}</option>
-                    {renderYearOptions()}
-                </select>
+                <div className={`${styles.custom_select} ${styles.year_select} ${birthDontKnow ? styles.disabled : ''}`}>
+                    <div className={styles.selected_option} onClick={() => toggleDropdown('year')}>
+                        {year ? `${year}년` : '년'}
+                        <TbTriangleInvertedFilled className={styles.dropdown_icon} />
+                    </div>
+                    <div className={`${styles.options} ${dropdown.year ? styles.show : ''}`}>
+                        {renderYearOptions()}
+                    </div>
                 </div>
-                <div className={styles.month_day_wrapper}>
-                <select className={styles.birth_select} value={month} onChange={handleMonthChange} disabled={!year || birthDontKnow}>
-                    <option value="">{(month === '') ? currentMonth + '월' : month}</option>
-                    {renderMonthOptions()}
-                </select>
+                <div className={`${styles.custom_select} ${styles.month_day_select} ${birthDontKnow || !year ? styles.disabled : ''}`}>
+                    <div className={styles.selected_option} onClick={() => toggleDropdown('month')}>
+                        {month ? `${month}월` : '월'}
+                        <TbTriangleInvertedFilled className={styles.dropdown_icon} />
+                    </div>
+                    <div className={`${styles.options} ${dropdown.month ? styles.show : ''}`}>
+                        {renderMonthOptions()}
+                    </div>
                 </div>
-                <div className={styles.month_day_wrapper}>
-                <select className={styles.birth_select} value={day} onChange={handleDayChange} disabled={!month || birthDontKnow}>
-                    <option value="">{(day === '') ? currentDay + '일' : day}</option>
-                    {renderDayOptions()}
-                </select>
+                <div className={`${styles.custom_select} ${styles.month_day_select} ${birthDontKnow || !month ? styles.disabled : ''}`}>
+                    <div className={styles.selected_option} onClick={() => toggleDropdown('day')}>
+                        {day ? `${day}일` : '일'}
+                        <TbTriangleInvertedFilled className={styles.dropdown_icon} />
+                    </div>
+                    <div className={`${styles.options} ${dropdown.day ? styles.show : ''}`}>
+                        {renderDayOptions()}
+                    </div>
                 </div>
             </div>
             <div className={styles.dontknowbirth_wrapper}>
-                    <span className={styles.dontknowbirth_text}>생일을 몰라요 </span>
-                    <input className={styles.dontknowbirth_checkbox} type='checkbox' onChange={dontKnowToggle} checked={birthDontKnow} />
+                <span className={styles.dontknowbirth_text}>생일을 몰라요 </span>
+                <input className={styles.dontknowbirth_checkbox} type='checkbox' onChange={dontKnowToggle} checked={birthDontKnow} />
             </div>
         </div>
     );
--- react/src/page/myPage.js
+++ react/src/page/myPage.js
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from "react";
 import ManageBox from "../component/MyPageComp/ManageBox";
 import { MdOutlinePets } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
 
 const MyPage = () => {
+
+  const Profile = () => {
+    return (
+      <div>
+        <div>
+          <div>
+            <img src="https://i.pinimg.com/564x/87/09/e7/8709e78fb7d788bb86bbc63be55f3184.jpg"/>
+          </div>
+        </div>
+        <MdEdit />
+      </div>
+    )
+  }
+
   return (
     <div>
+      <div>마이페이지</div>
+      <Profile />
       <ManageBox
         icon={<MdOutlinePets />}
         managementItemTitle={"내 동물 관리"}
       />
-      <ManageBox icon={<MdOutlinePets />} managementItemTitle={"내 글 보기"} />
+      <ManageBox 
+        icon={<MdOutlinePets />} 
+        managementItemTitle={"내 글 보기"} 
+      />
       <ManageBox
         icon={<MdOutlinePets />}
         managementItemTitle={"내 댓글 보기"}
