b746d7e04b4c0d979299e83b38560bc7e6c3ae81
WonYunSun <ereunosun113@gmail.com>
2024-07-22 12:25:42 +0900

    feat: schedule

--- react/src/css/globalStyle.js
+++ react/src/css/globalStyle.js
@@ -17,6 +17,19 @@ select{
 button {
     font-family: "SUIT Variable", sans-serif;
 }
+button {
+  outline: none; /* 포커스 시 생기는 테두리 제거 */
+  background-color: initial;
+}
+
+button:focus {
+  outline: none; /* 포커스 시 생기는 테두리 제거 */
+}
+
+button:active {
+  outline: none; /* 클릭 시 생기는 테두리 제거 */
+  background-color: initial; /* 클릭 시 배경색 변경 방지 */
+}
 `;
 
 export default GlobalStyle;
