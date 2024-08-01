35b9241976aa62243b2c9efd4cafdde8822beb54
leejh0315 <yijh0315@naver.com>
2024-08-01 21:05:43 +0900

    feat : backAddress

--- react/src/api/createAxios.js
+++ react/src/api/createAxios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const createAxios = (middlePath = "") => {
   return axios.create({
     // baseURL: `http://localhost:8081/api${middlePath}`,
-    baseURL: `http://175.45.202.131:8081/api${middlePath}`,
+    baseURL: `http://localhost:8081/api${middlePath}`,
     headers: {
       "Content-Type": "application/json",
     },
