686790ee2485f2709498de768bfe8482da9df184
HCWDDD <codn1016@gmail.com>
2024-08-05 21:35:08 +0900

    feat: SeeMyComments css

--- react/src/component/MyPageComp/MyPageCommonTopBar.js
+++ react/src/component/MyPageComp/MyPageCommonTopBar.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { GoArrowLeft } from "react-icons/go";
-import styles from "../../css/mypage_common_topbar.module.css"
+import styles from "../../css/mypage_common_topbar.module.css";
 
 const MyPageCommonTopBar = ({ title }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const goBack = () => {
-        navigate(-1); // 뒤로가기
-    };
+  const goBack = () => {
+    navigate(-1); // 뒤로가기
+  };
 
-    return (
-        <div className={styles.wrapper}>
-            <GoArrowLeft className={styles.back_icon} onClick={goBack} />
-            <div className={styles.page_title}>{title}</div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.inner_wrapper}>
+        <GoArrowLeft className={styles.back_icon} onClick={goBack} />
+        <div className={styles.page_title}>{title}</div>
+      </div>
+    </div>
+  );
+};
 
 export default MyPageCommonTopBar;
