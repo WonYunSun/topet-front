2a34aea422089949540ae50e0153d81ea62f619d
WonYunSun <ereunosun113@gmail.com>
2024-08-08 15:33:54 +0900

    feat : addshorts

--- react/src/component/Content.js
+++ react/src/component/Content.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import '../css/community_write_content.css'
+import React from "react";
+import "../css/community_write_content.css";
 
-const Content = ({value, handleContentTextChange }) => {
+const Content = ({
+  value,
+  handleContentTextChange,
+  maxLength,
+  placeholder,
+}) => {
   return (
-    <div className='contentwrite'>
-      <textarea 
-      placeholder="게시글을 입력해주세요" 
-      value={value} 
-      onChange={handleContentTextChange}
-      maxLength="1499" // 1500자로 설정하면, 영어는 1500자까지만 쳐지는데, 한글은 1501자까지 쳐짐
+    <div className="contentwrite">
+      <textarea
+        placeholder={placeholder}
+        value={value}
+        onChange={handleContentTextChange}
+        maxLength={maxLength} // 1500자로 설정하면, 영어는 1500자까지만 쳐지는데, 한글은 1501자까지 쳐짐
       ></textarea>
-      <div className='contentcount'>{value.length}/1500</div>
+      <div className="contentcount">
+        {value.length}/{maxLength + 1}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
