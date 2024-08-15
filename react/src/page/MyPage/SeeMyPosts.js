9514c4359749ea10915f5bb68a44cb931fcff58b
HCWDDD <codn1016@gmail.com>
2024-08-15 11:35:50 +0900

    feat: MyPage Web css

--- react/src/page/MyPage/SeeMyPosts.js
+++ react/src/page/MyPage/SeeMyPosts.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import MyPageCommonTopBar from "../../component/MyPageComp/MyPageCommonTopBar";
 import CommunityApi from "../../api/communityApi";
 import MyPostList from "../../component/MyPageComp/MyPostList";
-import styles from "../../css/communityList.module.css";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import MyPageSideBar from "../../component/MyPageComp/MyPageSideBar";
+import styles from "../../css/mypage_seemy.module.css";
+
+/// responsive
+import { Mobile, DeskTop } from "../../responsive/responsive";
+import { useMediaQuery } from "react-responsive";
 
 const SeeMyPosts = () => {
+  const isDeskTop = useMediaQuery({
+    query: "(min-width:769px)",
+  });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const isTablet = useMediaQuery({
+    query: "(min-width: 769px) and (max-width: 859px)",
+  });
+
   const reduxMember = useSelector((state) => state.member.member);
   const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
@@ -72,10 +85,17 @@ const SeeMyPosts = () => {
   // ))}
 
   return (
-    <div>
-      <MyPageCommonTopBar title={"내 게시글"} />
-      <MyPostList postType={"mypost"}/>
-    </div>
+    <>
+      <div className={`${isDeskTop && styles.wrapper_dtver}`}>
+        <div className={`${isDeskTop && styles.inner_wrapper}`}>
+          {isDeskTop && <MyPageSideBar option={"내 게시글 보기"} />}
+          <div className={`${isDeskTop && styles.rightside_wrapper}`}>
+            <MyPageCommonTopBar title={"내 게시글"} />
+            <MyPostList postType={"mypost"} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
