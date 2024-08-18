5f0f7094daea909499dcc4bef77a5d0dcd3e1e39
WonYunSun <ereunosun113@gmail.com>
2024-08-18 18:48:37 +0900

    feat : calendar css(web)

--- react/src/page/Calendarscreen.js
+++ react/src/page/Calendarscreen.js
@@ -186,7 +186,7 @@ const Calendarscreen = () => {
   if (!isLoaded) {
     return <div>Loading...</div>;
   }
-
+  console.log(isTablet);
   return (
     <>
       <Mobile>
@@ -198,10 +198,12 @@ const Calendarscreen = () => {
           (reduxPet == null ? (
             <></>
           ) : (
-            <AnimalSelect
-              onClick={handleOpenPetBottomSheet}
-              selectedPet={selectedPet}
-            />
+            <div className={`${isTablet && styles.tb_paddingdiv}`}>
+              <AnimalSelect
+                onClick={handleOpenPetBottomSheet}
+                selectedPet={selectedPet}
+              />
+            </div>
           ))}
 
         <BottomSheet
@@ -261,23 +263,31 @@ const Calendarscreen = () => {
         <DeskTop>
           {!isTablet ? (
             <div className={`${styles.CalendarscreenWrap_dtver}`}>
-              <div>
-                <Calendar schedules={schedules} onDateClick={handleDateClick} />
+              <div className={`${styles.leftWrapper}`}>
+                <div>캘린더</div>
               </div>
-              <div>
-                {reduxPet == null ? (
-                  <></>
-                ) : (
-                  <AnimalSelect
-                    onClick={handleOpenPetBottomSheet}
-                    selectedPet={selectedPet}
+              <div className={`${styles.rightWrapper}`}>
+                <div>
+                  <Calendar
+                    schedules={schedules}
+                    onDateClick={handleDateClick}
                   />
-                )}
-                <ScheduleBottom
-                  schedules={schedules}
-                  selectedDate={selectedDate}
-                  onScheduleClick={handleScheduleClick}
-                />
+                </div>
+                <div>
+                  {reduxPet == null ? (
+                    <></>
+                  ) : (
+                    <AnimalSelect
+                      onClick={handleOpenPetBottomSheet}
+                      selectedPet={selectedPet}
+                    />
+                  )}
+                  <ScheduleBottom
+                    schedules={schedules}
+                    selectedDate={selectedDate}
+                    onScheduleClick={handleScheduleClick}
+                  />
+                </div>
               </div>
             </div>
           ) : (
